aeecf1dfb98c58ef6c8adfcc015d6c24c6a79565
Nahid-Hassan <nahid.cseru@gmail.com>
2022-11-17 08:06:25 +0600

    simulation - lcm done

--- computer-simulation/lcm.py
+++ computer-simulation/lcm.py
@@ -8,10 +8,11 @@ m = 100     # modulus value
 total = 10
 
 print(x0, end=' ')
+total -= 1                  # seed is already a random number
 while total > 0:
-    xi = (x0 * a + c) % m
+    xi = (x0 * a + c) % m   # xi = (x0 * a + c) % m
     print(xi, end=' ')
-    x0 = xi
+    x0 = xi                 # x0 = xi 
     total -= 1
 
 print()
