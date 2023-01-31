72b57cbbd007c8611062759e9504d8fd44573598
Md. Nahid Hassan <36586531+Nahid-Hassan@users.noreply.github.com>
2023-01-31 05:05:40 +0000

    run-length

--- multimedia/run-length.cpp
+++ multimedia/run-length.cpp
@@ -48,3 +48,53 @@ int main() {
     string dtext = decode(etext, etext.size());
     cout << dtext << endl;
 }
+
+/*
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string encode(string text, int len) {
+    string etext = "";
+    int i, j, count;
+    for(i = 0; text[i]; i++){
+        count = 0;
+        for(j = i; text[j] && text[i]==text[j]; j++){
+            count++;
+        }
+        i = j - 1;
+        etext += text[i];
+        etext += '(' + to_string(count) + ')';
+    }
+
+    return etext;
+}
+
+string decode(string text, int len) {
+    string dtext = "";
+    for(int i = 0; text[i]; i++){
+        if(text[i] == '('){
+            int cnt = 0;
+            for(int j = i+1; text[j] && text[j] != ')'; j++){
+                cnt = cnt*10 + (int)(text[j] - '0');
+            }
+            for(int j = 0; j < cnt; j++){
+                dtext += text[i-1];
+            }
+        }
+    }
+
+    return dtext;
+}
+
+int main() {
+    string text = "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAADFdfsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    string etext = encode(text, text.size());
+    cout << etext << endl;
+
+    string dtext = decode(etext, etext.size());
+    cout << dtext << endl;
+}
+
+
+*/
