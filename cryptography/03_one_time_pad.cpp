1a39125304cc22b885a4149bd043d1780b028f95
Md. Nahid Hassan <36586531+Nahid-Hassan@users.noreply.github.com>
2023-02-01 07:10:02 +0000

    one time pad geeksforgeeks code

---
+++ cryptography/03_one_time_pad.cpp
@@ -0,0 +1,69 @@
+// C++ program Implementing One Time Pad Algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+string stringEncryption(string text, string key) {
+	string cipherText = "";
+	int cipher[key.length()];
+
+	for (int i = 0; i < key.length(); i++) {
+		cipher[i] = text.at(i) - 'A' + key.at(i) - 'A';
+	}
+
+	for (int i = 0; i < key.length(); i++) {
+		if (cipher[i] > 25) {
+			cipher[i] = cipher[i] - 26;
+		}
+	}
+
+	for (int i = 0; i < key.length(); i++) {
+		int x = cipher[i] + 'A';
+		cipherText += (char)x;
+	}
+
+	return cipherText;
+}
+
+static string stringDecryption(string s, string key) {
+	string plainText = "";
+	int plain[key.length()];
+
+	for (int i = 0; i < key.length(); i++) {
+		plain[i] = s.at(i) - 'A' - (key.at(i) - 'A');
+	}
+
+	for (int i = 0; i < key.length(); i++) {
+		if (plain[i] < 0) {
+			plain[i] = plain[i] + 26;
+		}
+	}
+
+	for (int i = 0; i < key.length(); i++) {
+		int x = plain[i] + 'A';
+		plainText += (char)x;
+	}
+
+	return plainText;
+}
+
+int main() {
+	string plainText = "ZZZZZ";
+	string key = "MONEY";
+
+	for (int i = 0; i < plainText.length(); i++) {
+		plainText[i] = toupper(plainText[i]);
+	}
+	for (int i = 0; i < key.length(); i++) {
+		key[i] = toupper(key[i]);
+	}
+	string encryptedText = stringEncryption(plainText, key);
+
+	cout << "Cipher Text - " << encryptedText << endl;
+
+	cout << "Message - "
+		<< stringDecryption(encryptedText, key);
+
+	return 0;
+}
+
