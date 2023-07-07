38da0bab472781865ad5903b8da950f6208b164c
Nahid-Hassan <nahid.cseru@gmail.com>
2023-07-07 22:21:14 +0600

    multimedia final (no error check)

---
+++ multimedia/01_RLE/main.py
@@ -0,0 +1,50 @@
+def rle_encode(data):
+    encoded = ""
+    prev_char = data[0]
+    count = 1
+
+    # aaabaa
+    for char in data[1:]:
+        if prev_char == char:
+            count += 1
+        else:
+            encoded += str(count) + prev_char
+            prev_char = char
+            count = 1
+    
+    encoded += str(count) + prev_char
+    return encoded
+
+
+def rle_decode(data):
+    decoded = ""
+    i = 0
+    while i < len(data):
+        count_str = ""
+        # 40a2c
+        while i < len(data) and data[i].isdigit():
+            count_str += data[i]
+            i += 1
+        count = int(count_str)
+        decoded += data[i] * count
+        i += 1
+    return decoded
+
+
+encoded_data = []
+with open("plain.txt", "r") as file:
+    for line in file.readlines():
+        encoded_data.append(rle_encode(line.rstrip()))
+
+with open("encoded.txt", "w") as file:
+    for line in encoded_data:
+        file.write(line + "\n")
+
+decoded_data = []
+with open("encoded.txt", "r") as file:
+    for line in file.readlines():
+        decoded_data.append(rle_decode(line.rstrip()))
+
+with open("decoded.txt", "w") as file:
+    for line in decoded_data:
+        file.write(line + "\n")
---
+++ multimedia/02_LZW/main.py
@@ -0,0 +1,80 @@
+# input: BABAABAAA
+"""
+     65  - A
+     66  - B
+66 - 256 - BA
+65 - 257 - AB
+256 
+"""
+def lzw_compress(data):
+    d = {chr(i): i for i in range(256)}
+    next_code = 256
+    compressed = []
+    cs = ""                             # current sequence
+    
+    # BABAA
+    for symbol in data:
+        ns = cs + symbol                # new sequence
+        if ns in d:                      
+            cs = ns
+        else:
+            compressed.append(d[cs])
+            d[ns] = next_code
+            next_code += 1
+            cs = symbol
+    
+    if cs in d:
+        compressed.append(d[cs])
+    
+    # print(compressed)
+    return compressed
+
+
+def lzw_decompress(compressed):
+    d = {i: chr(i) for i in range(256)}
+    next_code = 256
+    decompressed = ""
+    ps = chr(compressed[0])
+    decompressed += ps
+    
+    # 66 65 256 65
+    for code in compressed[1:]:
+        if code in d:
+            cs = d[code]
+        elif code == next_code:
+            cs = ps + ps[0]
+        else:
+            raise ValueError("Invalid compressed code")
+        
+        decompressed += cs
+        # print(code, cs, decompressed)
+        d[next_code] = ps + cs[0]
+        next_code += 1
+        ps = cs
+    
+    return decompressed
+
+
+# Read input from file
+with open('input.txt', 'r') as file:
+    input_data = file.read().rstrip()
+
+# Compress the data
+compressed_data = lzw_compress(input_data)
+
+# Write compressed data to file
+with open('compressed.txt', 'w') as file:
+    file.write(' '.join(map(str, compressed_data)))
+
+# Read compressed data from file
+with open('compressed.txt', 'r') as file:
+    compressed_data = list(map(int, file.read().split()))
+
+# Decompress the data
+decompressed_data = lzw_decompress(compressed_data)
+
+# Write decompressed data to file
+with open('decompressed.txt', 'w') as file:
+    file.write(decompressed_data)
+
+# print(len(compressed_data) / len(input_data))
