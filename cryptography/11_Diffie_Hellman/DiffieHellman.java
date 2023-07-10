a52317d60ebc34ecc9f4949154c68defc2aababb
Nahid-Hassan <nahid.cseru@gmail.com>
2023-07-10 10:50:37 +0600

    11 diffie hellman

---
+++ cryptography/11_Diffie_Hellman/DiffieHellman.java
@@ -0,0 +1,35 @@
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import javax.crypto.KeyAgreement;
+import javax.crypto.SecretKey;
+
+public class DiffieHellman {
+    public static void main(String[] args) throws Exception {
+        // Generate Alice's key pair
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("DH");
+        keyPairGenerator.initialize(1024);
+        KeyPair aliceKeyPair = keyPairGenerator.generateKeyPair();
+        
+        // Generate Bob's key pair
+        KeyPair bobKeyPair = keyPairGenerator.generateKeyPair();
+        
+        // Alice performs the key agreement with Bob's public key
+        KeyAgreement aliceKeyAgreement = KeyAgreement.getInstance("DH");
+        aliceKeyAgreement.init(aliceKeyPair.getPrivate());
+        aliceKeyAgreement.doPhase(bobKeyPair.getPublic(), true);
+        
+        // Bob performs the key agreement with Alice's public key
+        KeyAgreement bobKeyAgreement = KeyAgreement.getInstance("DH");
+        bobKeyAgreement.init(bobKeyPair.getPrivate());
+        bobKeyAgreement.doPhase(aliceKeyPair.getPublic(), true);
+        
+        // Generate the shared secret key for Alice and Bob
+        SecretKey aliceSharedSecretKey = aliceKeyAgreement.generateSecret("AES");
+        SecretKey bobSharedSecretKey = bobKeyAgreement.generateSecret("AES");
+        
+        // Compare the shared secret keys to ensure they match
+        boolean keysMatch = aliceSharedSecretKey.equals(bobSharedSecretKey);
+        
+        System.out.println("Shared Secret Keys Match: " + keysMatch);
+    }
+}
