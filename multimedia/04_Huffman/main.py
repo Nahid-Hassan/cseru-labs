0dc2967e7a966d8bca502d8225cb37d451aceef1
Nahid-Hassan <nahid.cseru@gmail.com>
2023-07-12 10:20:47 +0600

    huffman encode and decode added

--- multimedia/04_Huffman/main.py
+++ multimedia/04_Huffman/main.py
@@ -14,19 +14,22 @@ class node:
     def __lt__(self, nxt):
         return self.freq < nxt.freq
 
-xx = open("output.txt", "w")
 
+output = open("output.txt", "w")
 encode_dict, decode_dict = {}, {}
+
 
 def printNodes(node, val=""):
     newVal = val + str(node.huff)
+
     if node.left:
         printNodes(node.left, newVal)
     if node.right:
         printNodes(node.right, newVal)
 
     if not node.left and not node.right:
-        xx.write(f"{node.symbol} -> {newVal}" + "\n")
+        print(f"{node.symbol} -> {newVal}")
+        output.write(f"{node.symbol} -> {newVal}" + "\n")
         encode_dict[node.symbol] = newVal
         decode_dict[newVal] = node.symbol
 
@@ -36,30 +39,19 @@ def encode(msg):
         msg = msg.replace(key, val)
     return msg
 
+
 def decode(msg):
     for key, val in decode_dict.items():
-        msg = msg.replace(val, key)
-        print(key, val, msg)
+        msg = msg.replace(key, val)
     return msg
 
 
 with open("input.txt", "r") as f:
-    lines = f.readlines();
+    lines = f.readlines()
 
 nodes = []
 for key, val in Counter(lines[0]).items():
     heapq.heappush(nodes, node(val, key))
-
-
-
-# chars = ["a", "b", "c", "d", "e", "f"]
-# freq = [5, 9, 12, 13, 16, 45]
-
-# nodes = []
-# for x in range(len(chars)):
-#     heapq.heappush(nodes, node(freq[x], chars[x]))
-
-# nodes[o3, o4, o5, o6]
 
 while len(nodes) > 1:
     left = heapq.heappop(nodes)
@@ -71,11 +63,10 @@ while len(nodes) > 1:
     # combine the 2 smallest nodes to create
     # new node as their parent
     newNode = node(left.freq + right.freq, left.symbol + right.symbol, left, right)
-
     heapq.heappush(nodes, newNode)
 
 printNodes(nodes[0])
+
 out = encode(lines[0])
-print(out)
-xx.write(out + "\n")
-print(decode(out))
+print(f"After compressed string -> {lines[0]} - {out}.")
+print(f"After decompressing string -> {out} - {decode(out)}.")
